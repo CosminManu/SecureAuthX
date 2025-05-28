92638a77a87a27f45dd914df704bf66a8cc7494b
CosminManu <cosmin.manu2000@gmail.com>
2025-05-28 20:19:07 +0300

    authController in work, loginDto, registerDto

---
+++ SecureAuthX.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SecureAuthX.API.Data;
+using SecureAuthX.API.Models;
+
+namespace SecureAuthX.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly IConfiguration _config;
+
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration config)
+        {
+			_userManager = userManager;
+			_config = config;
+        }
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+		{
+			var user = new ApplicationUser
+			{
+				UserName = registerDto.Email,
+				Email = registerDto.Email,
+				FullName = registerDto.FullName,
+			};
+
+			var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+			if (!result.Succeeded)
+			{
+				return BadRequest(result.Errors);
+			}
+
+			return Ok("User registered successfully.");
+		}
+
+
+    }
+}
---
+++ SecureAuthX.API/Models/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SecureAuthX.API.Models
+{
+	public class LoginDto
+	{
+		public string Email {  get; set; }
+		public string Password { get; set; }
+	}
+}
