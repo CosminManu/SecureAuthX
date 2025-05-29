8ddcd60e20d3b8b9ab346ae3e3af936f8f3d3a59
CosminManu <cosmin.manu2000@gmail.com>
2025-05-29 23:56:08 +0300

    Login, GetMe, GEnerateJwtToken in authController

--- SecureAuthX.API/Controllers/AuthController.cs
+++ SecureAuthX.API/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SecureAuthX.API.Data;
 using SecureAuthX.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SecureAuthX.API.Controllers
 {
@@ -39,6 +44,54 @@ namespace SecureAuthX.API.Controllers
 			return Ok("User registered successfully.");
 		}
 
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+		{
+			var user = await _userManager.FindByEmailAsync(loginDto.Email);
 
-    }
+			if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+			{
+				return Unauthorized("Invalid credentials");
+			}
+
+			var token = GenerateJwtToken(user);
+
+			return Ok(new { token });
+		}
+
+		[Authorize]
+		[HttpGet("me")]
+		public async Task<IActionResult> GetMe()
+		{
+			var email = User.FindFirstValue(ClaimTypes.Email);
+			var user = await _userManager.FindByEmailAsync(email);
+
+			return Ok(new {user.FullName, user.Email});
+		}
+
+
+		private string GenerateJwtToken(ApplicationUser user)
+		{
+			var claims = new[]
+			{
+				new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+				new Claim(JwtRegisteredClaimNames.Email, user.Email),
+				new Claim(ClaimTypes.Email, user.Email),
+			};
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+			var expires = DateTime.Now.AddMinutes(30);
+
+			var token = new JwtSecurityToken(
+				issuer: _config["Jwt:Issuer"],
+				audience: _config["Jwt:Audience"],
+				claims: claims,
+				expires: expires,
+				signingCredentials: creds
+			);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+	}
 }
