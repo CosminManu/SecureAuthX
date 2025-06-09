e898bccadf299d6a6b2db9a65dbbf84680a31cdb
CosminManu <cosmin.manu2000@gmail.com>
2025-06-09 21:13:25 +0300

    pagina Me, UserDto, modificat NavMenu

---
+++ SecureAuthX.Client/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SecureAuthX.Client.Models
+{
+    public class UserDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
